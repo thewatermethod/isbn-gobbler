0d4a026477d82227169b4b0dc0f07079408668a0
Matt Bevilacqua <thewatermethod@gmail.com>
2023-01-08 18:37:03 -0500

    work on UI/UX

--- src/main.ts
+++ src/main.ts
@@ -9,43 +9,87 @@ interface BookData {
   dewey: string,
 }
 
+const bookTemplate =    `
+  <div class="book">
+    <img class="book-image" />
+    <h2 class="book-title"></h2>
+    <p class="book-author"></p>
+    <pre class="book-dewey"></pre>
+  </div>`;
+
+const form = document.querySelector('#form') as HTMLFormElement;
 const bar = document.querySelector('#code') as HTMLInputElement;
-const title = document.querySelector('#book-title') as HTMLDivElement;
-const image = document.querySelector('#book-image') as HTMLImageElement;
-const author = document.querySelector('#book-author') as HTMLDivElement;
-const dewey = document.querySelector('#book-dewey') as HTMLDivElement;
+const books = document.querySelector('#books') as HTMLDivElement;
+
 
 const fetchBookData = async (isbn: string) : Promise<BookData> => {
-
   const response = await fetch(`/gobble?isbn=${isbn.trim()}`);
-
-  const parsed = await response.json();
-  console.log(parsed);
-
-  return parsed;
+  const data = await response.json();
+  return data;
 }
 
-const renderBookData = (data: BookData) => {
-  title.innerText = data.title;
-  image.src = data.image;
-  author.innerText = data.author;
-  dewey.innerText = data.dewey;
+const addEventListeners = () => {
+  //todo: add event listeners to remove books
+};
+
+const renderBookData = (data: BookData,  isbn: string) => {
+
+  //first we can check to see if the book is already in the list
+  const existingBook = books.querySelector(`[data-isbn="${isbn}"]`);
+  if(existingBook) {
+    return;
+  }
+
+  // otherwise we can build a quick book template and add it to the list
+  const book = document.createElement('li');
+  book.innerHTML = bookTemplate;
+
+  const title = book.querySelector('.book-title') as HTMLHeadingElement;
+  if(title){
+    title.innerText = data.title;
+  }
+
+  const image = book.querySelector('.book-image') as HTMLImageElement;
+  if(image){
+    image.src = data.image;
+  }
+
+  const author = book.querySelector('.book-author') as HTMLParagraphElement;
+  if(author){
+    author.innerText = data.author;
+  }
+
+  const dewey = book.querySelector('.book-dewey') as HTMLPreElement;
+  if(dewey){
+    dewey.innerText = data.dewey;
+  }
+
+  book.dataset.isbn = isbn;
+
+  books.appendChild(book);
+
+  form.clear();
+
+  addEventListeners();
+}
+
+form.addEventListener('submit', async (e: Event) => {
+  e.preventDefault();
+
+  const isbn = bar.value;
+
+  if(!testISBN(isbn)) {
+    // todo: show error in UI
+    return;
+  }
+
+  const bookData = await fetchBookData(isbn);
+  renderBookData(bookData, isbn);
   bar.value = '';
-}
+});
 
 bar.addEventListener('keydown', async (e: KeyboardEvent) => {
   if(e.key === 'Enter') {
-    console.log('Enter was pressed');
-    const target = e.target as HTMLInputElement;
-    const isbn = target.value;
-  
-    if(!testISBN(isbn)) {
-      // todo: show error in UI
-      return;
-    }
-
-    const bookData = await fetchBookData(isbn);
-    renderBookData(bookData);
-    bar.value = '';
+    form.submit();
   }
 });
