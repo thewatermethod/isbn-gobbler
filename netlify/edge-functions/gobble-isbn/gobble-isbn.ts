b7abcd3dcbd5cf4e96ec8229e1626baefafd0f08
Matt Bevilacqua <thewatermethod@gmail.com>
2023-01-17 20:02:10 -0500

    Fix bugs, add loader

--- netlify/edge-functions/gobble-isbn/gobble-isbn.ts
+++ netlify/edge-functions/gobble-isbn/gobble-isbn.ts
@@ -1,8 +1,42 @@
 import testISBN from "../../../common/testISBN.ts";
 
-const url = (isbn: string) => `https://openlibrary.org/isbn/${isbn}.json`;
+interface OpenLibraryResponse {
+  full_title: string,
+  title: string,
+  by_statement: string,
+  dewey_decimal_class: string[],
+  lc_classifications: string[],
+}
 
-export default async (request: Request) => {
+/**
+ * 
+ * @param isbn {string}
+ * @returns {string} url string for openlibrary.org api endpoint
+ */
+const url = (isbn: string): string => `https://openlibrary.org/isbn/${isbn}.json`;
+
+/**
+ * 
+ * @param response {OpenLibraryResponse} response from openlibrary.org
+ * @returns {string} dewey decimal classification
+ */
+const extractDewey = (response: OpenLibraryResponse): string => {
+  const { dewey_decimal_class, lc_classifications } = response;
+  if(dewey_decimal_class) {
+    return dewey_decimal_class.join(', ');
+  }
+  if(lc_classifications) {
+    return lc_classifications.join(', ');
+  }
+  return '';
+};  
+
+/**
+ * 
+ * @param request {Request} request from netlify edge function
+ * @returns {Promise<Response>} response to netlify edge function
+ */
+export default async (request: Request): Promise<Response> => {
   const reqUrl = new URL(request.url);
   const isbn = reqUrl.searchParams.get("isbn");
 
@@ -16,18 +50,17 @@ export default async (request: Request) => {
     url(isbn)
   );
 
-  const parsed = await response.json();
+  const parsed: OpenLibraryResponse = await response.json();
 
-  const { full_title, title, by_statement, dewey_decimal_class, ...raw } = parsed;
+  const { full_title, title, by_statement } = parsed;
 
   const bookData = {
     title:  full_title || title,
     author: by_statement || '',
     image: "",
-    dewey: dewey_decimal_class ? dewey_decimal_class.join(', ') : "",
+    dewey: extractDewey(parsed),
     isbn: isbn,
     date: new Date().toISOString(),
-    raw,
   };
 
   return new Response(
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import testISBN from '../common/testISBN';
-import BookData from '../common/BookData';
-import './style.css'
+import testISBN from "../common/testISBN";
+import BookData from "../common/BookData";
+import "./style.css";
 
 const data: Array<BookData> = [];
-let sortKey = 'date-added';
+let sortKey = "date-added";
 
-const bookTemplate =    `
+const bookTemplate = `
   <div class="book">
     <h2 class="book-title"></h2>
     <p class="book-author"></p>
@@ -19,173 +19,201 @@ const removeButtonTemplate = (title: string) => `
   </button>
 `;
 
-const form = document.querySelector('#form') as HTMLFormElement;
-const bar = document.querySelector('#code') as HTMLInputElement;
-const books = document.querySelector('#books') as HTMLDivElement;
-const messageContainer = document.querySelector('#message') as HTMLDivElement;
-const csvButton = document.querySelector('#export') as HTMLButtonElement;
-const sortSelect = document.querySelector('#sort') as HTMLSelectElement;
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.querySelector("body") as HTMLBodyElement;
+  const form = document.querySelector("#form") as HTMLFormElement;
+  const bar = document.querySelector("#code") as HTMLInputElement;
+  const books = document.querySelector("#books") as HTMLDivElement;
+  const messageContainer = document.querySelector("#message") as HTMLDivElement;
+  const csvButton = document.querySelector("#export") as HTMLButtonElement;
+  const sortSelect = document.querySelector("#sort") as HTMLSelectElement;
+  const sortForm = document.querySelector("#sort-form") as HTMLFormElement;
 
-const sortData = () => {
-  switch(sortKey) {
-    case 'title':
-      data.sort((a, b) => a.title.localeCompare(b.title));
-      break;
-    case 'author':
-      data.sort((a, b) => a.author.localeCompare(b.author));
-      break;
-    case 'ddc':
-      debugger;
-      data.sort((a, b) => a.dewey.localeCompare(b.dewey));
-      break;
-    case 'date-added':
-      data.sort((a, b) => a.date.localeCompare(b.date));
-      break;
-  }
-}
+  sortForm.addEventListener("submit", (e: Event) => e.preventDefault());
 
-const fetchBookData = async (isbn: string) : Promise<BookData> => {
-  const response = await fetch(`/gobble?isbn=${isbn.trim()}`);
-  const data = await response.json();
-  return data;
-}
+  const setLoading = (loading: boolean) => {
+    if (loading) {
+      body.classList.add("loading");
+      // bar.setAttribute('disabled', 'true');
+      // csvButton.setAttribute('disabled', 'true');
+      // sortSelect.setAttribute('disabled', 'true');
+      // sortForm.setAttribute('disabled', 'true');
+      // form.setAttribute('disabled', 'true');
+    } else {
+      body.classList.remove("loading");
+    }
+  };
 
-const renderMessage = (message: string) => {
-  messageContainer.innerHTML = message;
-}
+  const sortData = () => {
+    switch (sortKey) {
+      case "title":
+        data.sort((a, b) => a.title.localeCompare(b.title));
+        break;
+      case "author":
+        data.sort((a, b) => a.author.localeCompare(b.author));
+        break;
+      case "ddc":
+        debugger;
+        data.sort((a, b) => a.dewey.localeCompare(b.dewey));
+        break;
+      case "date-added":
+        data.sort((a, b) => a.date.localeCompare(b.date));
+        break;
+    }
+  };
 
-const renderCSV = () => {
-  if(!data.length) {
-    renderMessage('No books to export! Add some books to your list first.');
-    return;
-  }
+  const fetchBookData = async (isbn: string): Promise<BookData> => {
+    const response = await fetch(`/gobble?isbn=${isbn.trim()}`);
+    const data = await response.json();
+    return data;
+  };
 
-  const csv = data.map((book) => {
-    return `"${book.title}","${book.author}","${book.dewey}","${book.isbn}"`;
-  });
+  const renderMessage = (message: string) => {
+    messageContainer.innerHTML = message;
+  };
 
-  const csvString = csv.join('\n');
-  
-  window.open(  
-    encodeURI(`data:text/csv;charset=utf-8,title,author,dewey,isbn\n${csvString}`)
-  );
-};
+  const renderCSV = () => {
+    if (!data.length) {
+      renderMessage("No books to export! Add some books to your list first.");
+      return;
+    }
 
-csvButton.addEventListener('click', renderCSV);
-
-const addEventListeners = (listItem: HTMLElement) => {
-  // add event listeners to remove books
-  const removeButton = listItem.querySelector('.book-remove') as HTMLButtonElement;
-  if(removeButton) {
-    removeButton.addEventListener('click', () => {
-      const isbn = listItem.dataset.isbn;
-      if(isbn) {
-        data.splice(data.findIndex((book) => book.isbn === isbn), 1);
-        renderBookData();
-      }
+    const csv = data.map((book) => {
+      return `"${book.title}","${book.author}","${book.dewey}","${book.isbn}"`;
     });
-  }
-};
 
-const renderBookData = () => {
-  books.innerHTML = '';
-  messageContainer.innerHTML =''; // clear any messages
+    const csvString = csv.join("\n");
 
-  // if there's no data, hide the sort select
-  if(!data.length) {
-    sortSelect.style.display = 'none';
-  } else {
-    sortSelect.style.display = 'block';
-  }
+    window.open(
+      encodeURI(
+        `data:text/csv;charset=utf-8,title,author,dewey,isbn\n${csvString}`
+      )
+    );
+  };
 
-  data.forEach((book) => {
-  // otherwise we can build a quick book template and add it to the list
-  const listItem = document.createElement('li');
-  listItem.innerHTML = bookTemplate;
+  csvButton.addEventListener("click", renderCSV);
 
-  listItem.setAttribute('role', 'listitem');
+  const addEventListeners = (listItem: HTMLElement) => {
+    // add event listeners to remove books
+    const removeButton = listItem.querySelector(
+      ".book-remove"
+    ) as HTMLButtonElement;
+    if (removeButton) {
+      removeButton.addEventListener("click", () => {
+        const isbn = listItem.dataset.isbn;
+        if (isbn) {
+          data.splice(
+            data.findIndex((book) => book.isbn === isbn),
+            1
+          );
+          renderBookData();
+        }
+      });
+    }
+  };
 
-  const title = listItem.querySelector('.book-title') as HTMLHeadingElement;
-  if(title){
-    title.innerHTML = `${book.title} ${removeButtonTemplate(book.title)}`;
-  }
+  const renderBookData = () => {
+    books.innerHTML = "";
+    messageContainer.innerHTML = ""; // clear any messages
 
-  const author = listItem.querySelector('.book-author') as HTMLParagraphElement;
-  if(author){
-    author.innerText = book.author;
-  }
+    // if there's no data, hide the sort select
+    if (!data.length) {
+      sortForm.style.display = "none";
+    } else {
+      sortForm.style.display = "block";
+    }
 
-  const dewey = listItem.querySelector('.book-dewey') as HTMLPreElement;
-  if(dewey){
-    dewey.innerText = book.dewey;
-  }
+    data.forEach((book) => {
+      // otherwise we can build a quick book template and add it to the list
+      const listItem = document.createElement("li");
+      listItem.innerHTML = bookTemplate;
 
-  listItem.dataset.isbn = book.isbn;
+      listItem.setAttribute("role", "listitem");
 
-  books.appendChild(listItem);
-  addEventListeners(listItem);
+      const title = listItem.querySelector(".book-title") as HTMLHeadingElement;
+      if (title) {
+        title.innerHTML = `${book.title} ${removeButtonTemplate(book.title)}`;
+      }
+
+      const author = listItem.querySelector(
+        ".book-author"
+      ) as HTMLParagraphElement;
+      if (author) {
+        author.innerText = book.author;
+      }
+
+      const dewey = listItem.querySelector(".book-dewey") as HTMLPreElement;
+      if (dewey) {
+        dewey.innerText = book.dewey;
+      }
+
+      listItem.dataset.isbn = book.isbn;
+
+      books.appendChild(listItem);
+      addEventListeners(listItem);
+    });
+  };
+
+  form.addEventListener("submit", async (e: Event) => {
+    e.preventDefault();
+
+    const isbn = bar.value;
+
+    if (!testISBN(isbn)) {
+      renderMessage("Please enter a valid ISBN");
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const bookData = await fetchBookData(isbn);
+
+      const exists = data.find((book) => book.isbn === isbn);
+
+      if (!exists) {
+        data.push(bookData);
+        localStorage.setItem("data", JSON.stringify(data));
+        renderBookData();
+      } else {
+        renderMessage("This book is already in your list!");
+        return;
+      }
+    } catch (error) {
+      renderMessage("There was an error fetching your book. Please try again.");
+    } finally {
+      setLoading(false);
+      // clear our form
+      form.reset();
+    }
   });
-}
 
-form.addEventListener('submit', async (e: Event) => {
-  e.preventDefault();
+  sortSelect.addEventListener("change", (e: Event) => {
+    const target = e.target as HTMLSelectElement;
+    const selected = target.value;
+    sortKey = selected;
+    localStorage.setItem("sort", selected);
 
-  const isbn = bar.value;
+    // sort our data
+    sortData();
 
-  if(!testISBN(isbn)) {
-    renderMessage('Please enter a valid ISBN');
-    return;
-  }
-
-  const bookData = await fetchBookData(isbn);
-
-  const exists = data.find((book) => book.isbn === isbn);
-
-  if(!exists) {
-    data.push(bookData);
-    localStorage.setItem('data', JSON.stringify(data));
+    // re-render our data
     renderBookData();
-  } else {
-    renderMessage('This book is already in your list!');
-    return;
-  }
+  });
 
-  // clear our form
-  form.reset();
-});
-
-bar.addEventListener('keydown', async (e: KeyboardEvent) => {
-  if(e.key === 'Enter') {
-    form.submit();
-  }
-});
-
-sortSelect.addEventListener('change', (e: Event) => {
-  const target = e.target as HTMLSelectElement;
-  const selected = target.value;
-  sortKey = selected;
-  localStorage.setItem('sort', selected);
-
-  // sort our data
-  sortData();
-
-  // re-render our data
-  renderBookData();
-});
-
-document.addEventListener('DOMContentLoaded', () => {
   // check for existing data
-  const existingData = localStorage.getItem('data');
-  if(existingData) {
+  const existingData = localStorage.getItem("data");
+  if (existingData) {
     data.push(...JSON.parse(existingData));
     renderBookData();
   }
 
-  const existingSort = localStorage.getItem('sort');
-  if(existingSort) {
+  const existingSort = localStorage.getItem("sort");
+  if (existingSort) {
     sortSelect.value = existingSort;
     sortKey = existingSort;
   }
 
-  sortSelect.querySelector(`option[value="${sortKey}"]`)?.setAttribute('selected', 'selected');
+  sortSelect
+    .querySelector(`option[value="${sortKey}"]`)
+    ?.setAttribute("selected", "selected");
 });
